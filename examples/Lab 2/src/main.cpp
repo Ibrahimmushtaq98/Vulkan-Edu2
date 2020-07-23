7a29ce681b099977262eb4d7194f9c9bfc7d2fe3
Ibrahimmushtaq98 <ibrahim.mushtaq@ontariotechu.net>
2020-07-23 19:25:10 -0400

    Finally Added Lab 2

--- examples/Lab 2/src/main.cpp
+++ examples/Lab 2/src/main.cpp
@@ -5,15 +5,22 @@
 
 #include <Vulkan_Edu.h>
 #include <cube_data.h>
+#include <tiny_obj_loader.h>
 
+//#define CUBE_MESH
+#define OBJ_MESH
 #define WIDTH 512
 #define HEIGHT 512
 
+
 //Custom States depending on what is needed
 struct appState {
+
+	//Vertex buffer
 	struct vertices v;
 	// Index buffer
 	struct indices i;
+	float* vBuffer;
 
 	// Uniform buffer block object
 	struct {
@@ -27,12 +34,6 @@ struct appState {
 		glm::mat4 modelMatrix;
 		glm::mat4 viewMatrix;
 	} uboVS;
-
-	glm::mat4 Projection;
-	glm::mat4 Clip;
-
-	float* vBuffer;
-	VkBuffer iBuffer;
 
 	VkPipelineLayout pipelineLayout;
 	VkPipeline pipeline;
@@ -53,6 +54,7 @@ float eyex, eyey, eyez;	// current user position
 
 double theta, phi;		// user's position  on a sphere centered on the object
 double r;
+int triangles;			// number of triangles
 
 void buildCommandBuffers(struct LHContext& context, struct appState& state) {
 	VkResult U_ASSERT_ONLY res;
@@ -89,14 +91,19 @@ void buildCommandBuffers(struct LHContext& context, struct appState& state) {
 		createScisscor(context, context.cmdBuffer[i], scissor);
 
 		vkCmdBindDescriptorSets(context.cmdBuffer[i], VK_PIPELINE_BIND_POINT_GRAPHICS, state.pipelineLayout, 0, 1, &state.descriptorSet, 0, nullptr);
-
 		vkCmdBindPipeline(context.cmdBuffer[i], VK_PIPELINE_BIND_POINT_GRAPHICS, state.pipeline);
 
 		VkDeviceSize offsets[1] = { 0 };
+#ifdef CUBE_MESH
 		vkCmdBindVertexBuffers(context.cmdBuffer[i], 0, 1, &state.v.buffer, offsets);
+		vkCmdBindIndexBuffer(context.cmdBuffer[i], state.i.buffer, 0, VK_INDEX_TYPE_UINT16);
+		vkCmdDrawIndexed(context.cmdBuffer[i], 36, 1, 0, 0, 0);
+#else
+		vkCmdBindVertexBuffers(context.cmdBuffer[i], 0, 1, &state.v.buffer, offsets);
+		vkCmdBindIndexBuffer(context.cmdBuffer[i], state.i.buffer, 0, VK_INDEX_TYPE_UINT32);
+		vkCmdDrawIndexed(context.cmdBuffer[i], state.i.count, 1, 0, 0, 0);
 
-		vkCmdDraw(context.cmdBuffer[i], 12 * 3, 1, 0, 0);
-
+#endif
 		vkCmdEndRenderPass(context.cmdBuffer[i]);
 
 		res = (vkEndCommandBuffer(context.cmdBuffer[i]));
@@ -353,44 +360,271 @@ void prepareUniformBuffers(struct LHContext& context, struct appState& state) {
 	updateUniformBuffers(context, state);
 }
 
+#ifdef CUBE_MESH
 void prepareVertices(struct LHContext& context, struct appState& state, bool useStagingBuffers) {
 	VkResult U_ASSERT_ONLY res;
 	bool U_ASSERT_ONLY pass;
 
-	VkMemoryAllocateInfo memAlloc = {};
-	memAlloc.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-	VkMemoryRequirements memReqs;
-	void* data;
+	VkBufferCreateInfo buf_info = {};
+	VkMemoryRequirements mem_reqs;
+	VkMemoryAllocateInfo alloc_info = {};
+	uint8_t* pData;
 
-	mapVerticiesToGPU(context, g_vb_solid_face_colors_Data, sizeof(g_vb_solid_face_colors_Data), state.v.buffer, state.v.memory);
+	int i;
+	int k;
+	float vertices[8][4] = {
+		{ -1.0, -1.0, -1.0, 1.0 }, //0
+		{ -1.0, -1.0, 1.0, 1.0 }, //1
+		{ -1.0, 1.0, -1.0, 1.0 }, //2
+		{ -1.0, 1.0, 1.0, 1.0 }, //3
+		{ 1.0, -1.0, -1.0, 1.0 }, //4
+		{ 1.0, -1.0, 1.0, 1.0 }, //5
+		{ 1.0, 1.0, -1.0, 1.0 }, //6
+		{ 1.0, 1.0, 1.0, 1.0 } //7
+	};
 
-	// Vertex input descriptions 
-	// Specifies the vertex input parameters for a pipeline
+	float normals[8][3] = {
+		{ -1.0, -1.0, -1.0 }, //0
+		{ -1.0, -1.0, 1.0 }, //1
+		{ -1.0, 1.0, -1.0 }, //2
+		{ -1.0, 1.0, 1.0 }, //3
+		{ 1.0, -1.0, -1.0 }, //4
+		{ 1.0, -1.0, 1.0 }, //5
+		{ 1.0, 1.0, -1.0 }, //6
+		{ 1.0, 1.0, 1.0 } //7
+	};
 
-	// Vertex input binding
-	// This example uses a single vertex input binding at binding point 0 (see vkCmdBindVertexBuffers)
+	uint16_t indices[36] = { 3, 1, 0, 0, 2, 3,
+		0, 4, 5, 5, 1, 0,
+		2, 6, 7, 7, 3, 2,
+		0, 4, 6, 0, 2, 6,
+		1, 5, 7, 7, 3, 1,
+		7, 5, 4, 4, 6, 7 };
+
+	state.vBuffer = new float[7 * 8];
+	k = 0;
+	for (i = 0; i < 8; i++) {
+		state.vBuffer[k++] = vertices[i][0];
+		state.vBuffer[k++] = vertices[i][1];
+		state.vBuffer[k++] = vertices[i][2];
+		state.vBuffer[k++] = vertices[i][3];
+		state.vBuffer[k++] = normals[i][0];
+		state.vBuffer[k++] = normals[i][1];
+		state.vBuffer[k++] = normals[i][2];
+	}
+
+	uint32_t dataSize = 7 * 8 * sizeof(float);
+	uint32_t dataStride = 7 * sizeof(float);
+
+	//mapIndiciesToGPU(context, indices, sizeof(indices), state.i.buffer, state.i.memory);
+	//mapVerticiesToGPU(context, state.vBuffer, dataSize, state.v.buffer, state.v.memory);
+
+	buf_info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	buf_info.pNext = NULL;
+	buf_info.usage = VK_BUFFER_USAGE_INDEX_BUFFER_BIT;
+	buf_info.size = sizeof(indices);
+	buf_info.queueFamilyIndexCount = 0;
+	buf_info.pQueueFamilyIndices = NULL;
+	buf_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	buf_info.flags = 0;
+	res = vkCreateBuffer(context.device, &buf_info, NULL, &state.i.buffer);
+	assert(res == VK_SUCCESS);
+	vkGetBufferMemoryRequirements(context.device, state.i.buffer, &mem_reqs);
+	alloc_info.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	alloc_info.pNext = NULL;
+	alloc_info.memoryTypeIndex = 0;
+	alloc_info.allocationSize = mem_reqs.size;
+	pass = memory_type_from_properties(context, mem_reqs.memoryTypeBits,
+		VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+		VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+		&alloc_info.memoryTypeIndex);
+	assert(pass && "No mappable coherent memory");
+	VkDeviceMemory mem;
+	res = vkAllocateMemory(context.device, &alloc_info, NULL, &mem);
+	assert(res == VK_SUCCESS);
+	res = vkMapMemory(context.device, mem, 0, mem_reqs.size, 0, (void**)
+		&pData);
+	assert(res == VK_SUCCESS);
+	memcpy(pData, indices, sizeof(indices));
+	vkUnmapMemory(context.device, mem);
+	res = vkBindBufferMemory(context.device, state.i.buffer, mem, 0);
+	assert(res == VK_SUCCESS);
+
+	buf_info = {};
+	buf_info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	buf_info.pNext = NULL;
+	buf_info.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+	buf_info.size = dataSize;
+	buf_info.queueFamilyIndexCount = 0;
+	buf_info.pQueueFamilyIndices = NULL;
+	buf_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	buf_info.flags = 0;
+	res = vkCreateBuffer(context.device, &buf_info, NULL, &state.v.buffer);
+	assert(res == VK_SUCCESS);
+
+	mem_reqs = {};
+	vkGetBufferMemoryRequirements(context.device, state.v.buffer,
+		&mem_reqs);
+
+	alloc_info = {};
+	alloc_info.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	alloc_info.pNext = NULL;
+	alloc_info.memoryTypeIndex = 0;
+
+	alloc_info.allocationSize = mem_reqs.size;
+	pass = memory_type_from_properties(context, mem_reqs.memoryTypeBits,
+		VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+		VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+		&alloc_info.memoryTypeIndex);
+	assert(pass && "No mappable, coherent memory");
+
+	res = vkAllocateMemory(context.device, &alloc_info, NULL,
+		&(state.v.memory));
+	assert(res == VK_SUCCESS);
+	state.v.buffer_info.range = mem_reqs.size;
+	state.v.buffer_info.offset = 0;
+
+	res = vkMapMemory(context.device, state.v.memory, 0, mem_reqs.size, 0,
+		(void**)&pData);
+	assert(res == VK_SUCCESS);
+
+	memcpy(pData, state.vBuffer, dataSize);
+
+	vkUnmapMemory(context.device, state.v.memory);
+
+	res = vkBindBufferMemory(context.device, state.v.buffer,
+		state.v.memory, 0);
+	assert(res == VK_SUCCESS);
+
+	//// Vertex input descriptions 
+	//// Specifies the vertex input parameters for a pipeline
+
+	//// Vertex input binding
+	//// This example uses a single vertex input binding at binding point 0 (see vkCmdBindVertexBuffers)
 
 	state.vertexInputBinding.binding = 0;
-	state.vertexInputBinding.stride = sizeof(g_vb_solid_face_colors_Data[0]);
 	state.vertexInputBinding.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+	state.vertexInputBinding.stride = dataStride;
+
 
 	// Inpute attribute bindings describe shader attribute locations and memory layouts
 	// These match the following shader layout
 	//	layout (location = 0) in vec3 inPos;
-	//	layout (location = 1) in vec3 inColor;
+	//	layout (location = 1) in vec3 inNormal;
 	// Attribute location 0: Position
+	//// Attribute location 1: Normal
 	state.vertexInputAttributs[0].binding = 0;
 	state.vertexInputAttributs[0].location = 0;
-	// Position attribute is three 32 bit signed (SFLOAT) floats (R32 G32 B32)
-	state.vertexInputAttributs[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+	state.vertexInputAttributs[0].format = VK_FORMAT_R32G32B32A32_SFLOAT;
 	state.vertexInputAttributs[0].offset = 0;
-	// Attribute location 1: Color
 	state.vertexInputAttributs[1].binding = 0;
 	state.vertexInputAttributs[1].location = 1;
-	// Color attribute is three 32 bit signed (SFLOAT) floats (R32 G32 B32)
 	state.vertexInputAttributs[1].format = VK_FORMAT_R32G32B32_SFLOAT;
 	state.vertexInputAttributs[1].offset = 16;
+
 }
+#endif
+
+#ifdef OBJ_MESH
+void prepareVertices(struct LHContext& context, struct appState& state, bool useStagingBuffers) {
+	VkResult U_ASSERT_ONLY res;
+	bool U_ASSERT_ONLY pass;
+
+	float* vertices;
+	float* normals;
+	uint32_t* indices;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	int nv;
+	int nn;
+	int ni;
+	int i;
+
+	VkBufferCreateInfo buf_info = {};
+	VkMemoryRequirements mem_reqs;
+	VkMemoryAllocateInfo alloc_info = {};
+	uint8_t* pData;
+
+	std::string err = tinyobj::LoadObj(shapes, materials, "data/sphere.obj", 0);
+
+	if (!err.empty()) {
+		std::cerr << err << std::endl;
+		return;
+	}
+
+	/*  Retrieve the vertex coordinate data */
+
+	nv = (int)shapes[0].mesh.positions.size();
+	vertices = new GLfloat[nv];
+	for (i = 0; i < nv; i++) {
+		vertices[i] = shapes[0].mesh.positions[i];
+	}
+
+	/*  Retrieve the vertex normals */
+
+	nn = (int)shapes[0].mesh.normals.size();
+	normals = new GLfloat[nn];
+	for (i = 0; i < nn; i++) {
+		normals[i] = shapes[0].mesh.normals[i];
+	}
+
+	/*  Retrieve the triangle indices */
+
+	ni = (int)shapes[0].mesh.indices.size();
+	triangles = ni / 3;
+	indices = new uint32_t[ni];
+	for (i = 0; i < ni; i++) {
+		indices[i] = shapes[0].mesh.indices[i];
+	}
+
+
+	state.vBuffer = new float[nv + nn];
+	int k = 0;
+	for (i = 0; i < nv / 3; i++) {
+		state.vBuffer[k++] = vertices[3 * i];
+		state.vBuffer[k++] = vertices[3 * i + 1];
+		state.vBuffer[k++] = vertices[3 * i + 2];
+		state.vBuffer[k++] = normals[3 * i];
+		state.vBuffer[k++] = normals[3 * i + 1];
+		state.vBuffer[k++] = normals[3 * i + 2];
+	}
+
+	uint32_t dataSize = (nv + nn) * sizeof(state.vBuffer[0]);
+	uint32_t dataStride = 6 * (sizeof( float));
+
+	state.i.count = ni;
+
+	mapIndiciesToGPU(context, indices, sizeof(indices[0]) * ni, state.i.buffer, state.i.memory);
+	mapVerticiesToGPU(context, state.vBuffer, dataSize, state.v.buffer, state.v.memory);
+
+	//// Vertex input descriptions 
+	//// Specifies the vertex input parameters for a pipeline
+
+	//// Vertex input binding
+	//// This example uses a single vertex input binding at binding point 0 (see vkCmdBindVertexBuffers)
+
+	state.vertexInputBinding.binding = 0;
+	state.vertexInputBinding.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+	state.vertexInputBinding.stride = dataStride;
+
+
+	// Inpute attribute bindings describe shader attribute locations and memory layouts
+	// These match the following shader layout
+	//	layout (location = 0) in vec3 inPos;
+	//	layout (location = 1) in vec3 inNormal;
+	// Attribute location 0: Position
+	//// Attribute location 1: Normal
+	state.vertexInputAttributs[0].binding = 0;
+	state.vertexInputAttributs[0].location = 0;
+	state.vertexInputAttributs[0].format = VK_FORMAT_R32G32B32A32_SFLOAT;
+	state.vertexInputAttributs[0].offset = 0;
+	state.vertexInputAttributs[1].binding = 0;
+	state.vertexInputAttributs[1].location = 1;
+	state.vertexInputAttributs[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+	state.vertexInputAttributs[1].offset = sizeof(float);
+
+}
+#endif // OBJ_MESH
 
 void renderLoop(struct LHContext& context, struct appState& state) {
 
